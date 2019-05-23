b8c2fd46bbf0265c7b3d021d99a2dec48d854b28
chong <304451676@qq.com>
2019-05-23 11:05:12 +0800

    machine-learning ex2

---
+++ machine-learning-ex2/ex2/predict.m
@@ -0,0 +1,39 @@
+function p = predict(theta, X)
+%PREDICT Predict whether the label is 0 or 1 using learned logistic 
+%regression parameters theta
+%   p = PREDICT(theta, X) computes the predictions for X using a 
+%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+
+m = size(X, 1); % Number of training examples
+
+% You need to return the following variables correctly
+p = zeros(m, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters. 
+%               You should set p to a vector of 0's and 1's
+%
+% X.shape = [m, n]; theta.shape = [n, 1];
+predict = sigmoid(X * theta);
+% predict.shape = [m, 1];
+for i = 1:m
+    if predict(i) >= 0.5
+        p(i) = 1;
+    else
+        p(i) = 0;
+    end;
+end;
+
+% 以下为参考别人的实现
+% prob = sigmoid(X * theta);
+% p(prob >= 0.5) = 1;
+% p(prob < 0.5)  = 0;
+
+
+
+
+% =========================================================================
+
+
+end
